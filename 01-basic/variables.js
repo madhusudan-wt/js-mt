27304a305fecbb54c18c53e649a39c10da640704
madhusudan-wt <madhusudan.webiators@gmail.com>
2025-06-11 05:04:50 +0000

    1st commit variable js

---
+++ 01-basic/variables.js
@@ -0,0 +1,9 @@
+const accountId = 12345
+let email = "test@gmail.com"
+var password = "1234"
+accountCity ="Jaipur"
+
+
+//console.log(accountId)
+
+console.table([accountId, email,password,accountCity ])
